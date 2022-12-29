ec059cdfefd8aab8858a25f52a9294fad62fa694
ZiadMysara <81532828+ZiadMysara@users.noreply.github.com>
2022-12-29 04:21:28 +0200

    mobile form

---
+++ persons-service/src/main/java/com/fci/fawrySystem/persons_service/models/forms/payment_forms/mobile_recharge_forms/VodafoneMobileChargePaymentForm.java
@@ -0,0 +1,5 @@
+package com.fci.fawrySystem.persons_service.models.forms.payment_forms.mobile_recharge_forms;
+
+import com.fci.fawrySystem.persons_service.models.forms.payment_forms.PaymentForm;
+
+public class VodafoneMobileChargePaymentForm extends PaymentForm{}
---
+++ persons-service/src/main/java/com/fci/fawrySystem/persons_service/models/forms/payment_forms/mobile_recharge_forms/WeMobileChargePaymentForm.java
@@ -0,0 +1,5 @@
+package com.fci.fawrySystem.persons_service.models.forms.payment_forms.mobile_recharge_forms;
+
+import com.fci.fawrySystem.persons_service.models.forms.payment_forms.PaymentForm;
+
+public class WeMobileChargePaymentForm extends PaymentForm{}
