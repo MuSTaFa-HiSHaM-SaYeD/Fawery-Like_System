863ce68b5c2deae0f8bfbccbf15c3886d60028c9
ZiadMysara <81532828+ZiadMysara@users.noreply.github.com>
2022-12-29 17:00:30 +0200

    i add discount forms now

---
+++ fawry-system/src/main/java/com/swAssignment/fawrysystem/models/discounts/OverallDiscount.java
@@ -0,0 +1,9 @@
+package com.fci.fawrySystem.persons_service.models.discounts;
+
+public class OverallDiscount extends Discount{
+
+	public OverallDiscount(String name, double percentage) {
+		super(name, percentage);
+	}
+	
+}
