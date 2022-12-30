2e19f9f2aa51edb3ec062a2b5cac3dafc46ec86c
Mostafa Hesham <95468778+mostafa20220@users.noreply.github.com>
2022-12-30 20:18:52 +0200

    added discount controller

---
+++ fawry-system/src/main/java/com/swAssignment/fawrysystem/controllers/DiscountController.java
@@ -0,0 +1,31 @@
+package com.swAssignment.fawrysystem.BSL.transactions;
+
+import java.util.ArrayList;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.swAssignment.fawrysystem.BSL.ServiceBsl;
+import com.swAssignment.fawrysystem.models.discounts.OverallDiscount;
+import com.swAssignment.fawrysystem.models.discounts.SpecificDiscount;
+
+@RestController
+public class DiscountController {
+	ServiceBsl serviceBsl;
+	public DiscountController(ServiceBsl serviceBsl) {
+		this.serviceBsl=serviceBsl;
+	}
+	@GetMapping(value = "/admin/discounts/overallDiscount/add/{percentage}")
+    public String addOverAllDiscount(@PathVariable("percentage") double percentage) {
+    	return serviceBsl.addDiscount("all", new OverallDiscount("overall",percentage));
+    }
+    @GetMapping(value = "/admin/discounts/specificDiscount/add/{percentage}/{serviceName}")
+    public String addSpecificDiscount(@PathVariable double percentage,@PathVariable String serviceName) {
+    	return serviceBsl.addDiscount(serviceName, new SpecificDiscount("specifc",percentage));
+    }
+    @GetMapping(value = "/discounts/get")
+    public ArrayList<String> getDiscounts(){
+    	return serviceBsl.GetDiscounts();
+    }
+}
